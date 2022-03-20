c7bfb6a388d0bbf3a3c3e8885991085930ce6c9a
Charchitg <Charchitg07@gmail.com>
2022-03-20 13:28:24 +0530

    User and Admin authentication completed

--- api/Controllers/Authentication.js
+++ api/Controllers/Authentication.js
@@ -1,11 +1,11 @@
 const User = require('../Models/User');
 const Admin = require('../Models/Admin');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const AdminValidation = require('../Helpers/ValidateAdmindetails');
-const UserValidation = require('../Helpers/ValidateUserdetails');
-const { VerifyToken } = require('../Helpers/TokenVerify');
-
+// const jwt = require('jsonwebtoken');
+// const AdminValidation = require('../Helpers/ValidateAdmindetails');
+// const UserValidation = require('../Helpers/ValidateUserdetails');
+// const { VerifyToken } = require('../Helpers/TokenVerify');
+const { GenerateToken } = require('../Helpers/TokenGenerator');
 
 
 
@@ -16,25 +16,64 @@ exports.adminRegistration = async (req, res, next) => {
 
         let errors = [];
 
-        if (AdminValidation.SameName(name)) {
+        // if (AdminValidation.SameName(name)) {
+        //     errors.push("Choose a different name");
+        // }
+        let exist;
+        exist = await Admin.findOne({ name: name });
+        if (exist) {
             errors.push("Choose a different name");
         }
-        if (AdminValidation.SameUsername(username)) {
+
+
+        // if (AdminValidation.SameUsername(username)) {
+        //     errors.push("Choose a different username");
+        // }
+
+        exist = await Admin.findOne({ username: username });
+        if (exist) {
             errors.push("Choose a different username");
         }
-        if (AdminValidation.InvalidEmail(email)) {
+        // if (AdminValidation.InvalidEmail(email)) {
+        //     errors.push("Given email is invalid");
+        // }
+
+        ans = email.toLowerCase().match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+        exist = !ans;
+        if (exist) {
             errors.push("Given email is invalid");
         }
-        if (AdminValidation.SameEmail(email)) {
+
+
+        // if (AdminValidation.SameEmail(email)) {
+        //     errors.push("Account exists with the given email , Choose a different email");
+        // }
+        exist = await Admin.findOne({ email: email });
+        if (exist) {
             errors.push("Account exists with the given email , Choose a different email");
         }
-        if (AdminValidation.ValidContactnum(contactnum)) {
+
+        // if (AdminValidation.ValidContactnum(contactnum)) {
+        //     errors.push("Given phone number is invalid");
+        // }
+        const len = contactnum.length;
+        if (len !== 10) {
             errors.push("Given phone number is invalid");
         }
-        if (AdminValidation.SameContactnum(contactnum)) {
-            errors.push("Account exists with the given phone number , Choose a different phone number")
+
+
+        // if (AdminValidation.SameContactnum(contactnum)) {
+        //     errors.push("Account exists with the given phone number admin, Choose a different phone number")
+        // }
+        exist = await Admin.findOne({ contactnum: contactnum });
+        if (exist) {
+            errors.push("Account exists with the given phone number admin, Choose a different phone number");
         }
-        if (password.length() <= 8) {
+
+
+        if (password.length <= 8) {
             errors.push("Too short password , enter atleast 8 Characters");
         }
 
@@ -42,19 +81,19 @@ exports.adminRegistration = async (req, res, next) => {
             errors.push("Password and Confirm Password do not match");
         }
 
-        if (address.length() <= 8) {
+        if (address.length <= 8) {
             errors.push("Too short address");
         }
 
-        if (errors.length() === 0) {
+        if (errors.length === 0) {
             const salt = await bcrypt.genSalt(10);
             const hash = await bcrypt.hash(password, salt);
-            const admin = new Admin({ name, username , email, contactnum, hash, address , minorderprice });
+            const admin = new Admin({ name, username, email, contactnum, hash, address, minorderprice });
 
             const token = await GenerateToken(admin);
 
             res.cookie("accesstoken", token, {
-                expires: process.env.EXPIRE_TIME,
+                expires: (new Date(Date.now() + 86400 * 1000)),
                 httpOnly: true
             });
 
@@ -68,6 +107,7 @@ exports.adminRegistration = async (req, res, next) => {
             });
         }
     } catch (error) {
+        console.log(error);
         res.status(401).json({
             message: error
         });
@@ -77,37 +117,74 @@ exports.adminRegistration = async (req, res, next) => {
 
 exports.userRegistration = async (req, res, next) => {
     try {
-        const { name, email, contactnum, password, confirm, address } = req.body;
+        let { name, email, contactnum, password, confirm, address } = req.body;
 
         let errors = [];
 
-        if (UserValidation.SameName(name)) {
+        // if (!UserValidation.SameName(name)) {
+        //     errors.push("Choose a different name");
+        // }
+
+
+        let exist = await User.findOne({ name: name });
+        if (exist !== null) {
+
             errors.push("Choose a different name");
         }
-        if (UserValidation.InvalidEmail(email)) {
+
+
+        // if (UserValidation.InvalidEmail(email)) {
+        //     errors.push("Given email is invalid");
+        // }
+
+        let ans = email.toLowerCase().match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+        exist = !ans;
+        if (exist) {
             errors.push("Given email is invalid");
         }
-        if (UserValidation.SameEmail(email)) {
+
+
+        // if (UserValidation.SameEmail(email)) {
+        //     errors.push("Account exists with the given email , Choose a different email");
+        // }
+        exist = await User.findOne({ email: email });
+        if (exist) {
             errors.push("Account exists with the given email , Choose a different email");
         }
-        if (UserValidation.ValidContactnum(contactnum)) {
+
+
+        // if ( UserValidation.ValidContactnum(contactnum)) {
+        //     errors.push("Given phone number is invalid");
+        // }
+        const len = contactnum.length;
+        if (len !== 10) {
             errors.push("Given phone number is invalid");
         }
-        if (UserValidation.SameContactnum(contactnum)) {
-            errors.push("Account exists with the given phone number , Choose a different phone number")
+
+
+        // if (UserValidation.SameContactnum(contactnum)) {
+        //     errors.push("Account exists with the given phone number , Choose a different phone number")
+        // }
+        exist = await User.findOne({ contactnum: contactnum });
+        if (exist) {
+            errors.push("Account exists with the given phone number , Choose a different phone number");
         }
-        if (password.length() <= 8) {
+
+
+        if (password.length <= 8) {
             errors.push("Too short password , enter atleast 8 Characters");
         }
 
         if (password !== confirm) {
             errors.push("Password and Confirm Password do not match");
         }
-        if (address.length() <= 8) {
+        if (address.length <= 8) {
             errors.push("Too short address");
         }
-
-        if (errors.length() === 0) {
+        //console.log(errors , typeof(errors));
+        if (errors.length === 0) {
             const salt = await bcrypt.genSalt(10);
             const hash = await bcrypt.hash(password, salt);
             const user = new User({ name, email, contactnum, hash, address });
@@ -115,7 +192,7 @@ exports.userRegistration = async (req, res, next) => {
             const token = await GenerateToken(user);
 
             res.cookie("accesstoken", token, {
-                expires: process.env.EXPIRE_TIME,
+                expires: (new Date(Date.now() + 86400 * 1000)),
                 httpOnly: true
             });
 
@@ -129,6 +206,7 @@ exports.userRegistration = async (req, res, next) => {
             });
         }
     } catch (error) {
+        console.log(error);
         res.status(401).json({
             message: error
         });
@@ -136,71 +214,73 @@ exports.userRegistration = async (req, res, next) => {
 }
 
 
-exports.userlogin = async(req,res,next) => {
+exports.userlogin = async (req, res, next) => {
     try {
-      
-        const {email , hash} = req.body;
 
-        const user = await User.findOne( { email : email });
-        const errors  = [];
-        if(!user){
+        const { email, hash } = req.body;
+
+        const user = await User.findOne({ email: email });
+        const errors = [];
+        if (!user) {
             errors.push("invalid username , no user found ");
         }
 
-        const comp = await bcrypt.compare(hash , user.hash );
+        const comp = await bcrypt.compare(hash, user.hash);
 
-        if(comp){
+        if (comp) {
             const token = await GenerateToken(user);
 
             res.cookie("accesstoken", token, {
-                expires: process.env.EXPIRE_TIME,
+                expires: (new Date(Date.now() + 86400 * 1000)),
                 httpOnly: true
             });
 
-            res.status(201).json({message : "Login Successful" , accesstoken : token });
+            res.status(201).json({ message: "Login Successful", accesstoken: token });
         }
-        else{
+        else {
             errors.push("Incorrect Password ");
-            res.status(200).json({errors : errors });
+            res.status(200).json({ errors: errors });
         }
     } catch (error) {
-        res.status(401).json({message : error });
+        console.log(error);
+        res.status(401).json({ message: error });
     }
 }
 
 
-exports.adminlogin = async (req,res,next) => {
+exports.adminlogin = async (req, res, next) => {
     try {
-        const {email , hash} = req.body;
+        const { email, hash } = req.body;
 
-        const admin = await Admin.findOne( { email : email });
-        const errors  = [];
-        if(!admin){
+        const admin = await Admin.findOne({ email: email });
+        const errors = [];
+        if (!admin) {
             errors.push("invalid username , no user found ");
         }
 
-        const comp = await bcrypt.compare(hash , user.hash );
+        const comp = await bcrypt.compare(hash, admin.hash);
 
-        if(comp){
+        if (comp) {
             const token = await GenerateToken(admin);
 
             res.cookie("accesstoken", token, {
-                expires: process.env.EXPIRE_TIME,
+                expires: (new Date(Date.now() + 86400 * 1000)),
                 httpOnly: true
             });
 
-            res.status(201).json({message : "Login Successful" , accesstoken : token });
+            res.status(201).json({ message: "Login Successful", accesstoken: token });
         }
-        else{
+        else {
             errors.push("Incorrect Password ");
-            res.status(200).json({errors : errors });
+            res.status(200).json({ errors: errors });
         }
     } catch (error) {
-        res.status(401).json({message : error });
+        console.log(error);
+        res.status(401).json({ message: error });
     }
 }
 
 
-exports.logout = async (req,res,next) => {
-    res.clearCookie("accesstoken").status(200).json({message : "logout successful "});
+exports.logout = async (req, res, next) => {
+    res.clearCookie("accesstoken").status(200).json({ message: "logout successful " });
 }
