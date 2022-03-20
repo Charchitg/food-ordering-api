b209b9b7694ce0ba135ce28aa0c3448cf0b1cba1
Charchitg <Charchitg07@gmail.com>
2022-03-20 13:32:21 +0530

    added add update and delete item route

--- api/Routes/Admin.js
+++ api/Routes/Admin.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const authentication = require('../Controllers/Authentication');
 const { authorization } = require('../Middlewares/authorization');
+const FoodItem = require('../Controllers/Fooditem');
 const router = express.Router();
+
+
+//  admin auth routes
 
 router.post('/login' , authentication.adminlogin);
 router.post('/register' , authentication.adminRegistration);
 router.post('/logout' , authorization , authentication.logout );
+
+// food item routes
+// router.post('mylist' , authorization , FoodItem.allitems);
+router.post('/mylist/add' , authorization , FoodItem.additem);
+router.post('/mylist/update' , authorization , FoodItem.updateitem);
+router.delete('/mylist/delete/:id' , authorization , FoodItem.deleteitem);
 
 
 module.exports = router;
